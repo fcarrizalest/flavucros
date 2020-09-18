bb1fe92ca2fcf47a44e7079b1d1323e8307f2284
Francisco Carrizales <francisco.carrizales@webpoint.mx>
2020-09-17 21:39:22 -0500

    Enviar mensaje al entrar a una url, cambio de flask por Klein

---
+++ web/README.md
@@ -0,0 +1,7 @@
+README.md
+
+
+https://klein.readthedocs.io/en/latest/introduction/2-twistdtap.html
+https://github.com/twisted/klein
+
+
--- web/app.py
+++ web/app.py
@@ -1,11 +1,37 @@
-from flask import Flask
-from flask import render_template
-app = Flask(__name__)
+import jinja2
+from klein import Klein
+
+from twisted.internet.defer import inlineCallbacks, returnValue
+from autobahn.twisted.wamp import Application
 
 
-@app.route('/')
-def hello():
+# This is our WAMP application
+##
+wampapp = Application('com.example')
 
 
-    
-    return render_template('index.html')
+# This is our Web application
+##
+webapp = Klein()
+webapp.visits = 0
+webapp.templates = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+
+
+@webapp.route('/')
+def home(request):
+    webapp.visits += 1
+    wampapp.session.publish('com.example.onvisit', webapp.visits)
+    page = webapp.templates.get_template('index.html')
+    return page.render(visits=webapp.visits)
+
+
+
+if __name__ == "__main__":
+    import sys
+    from twisted.python import log
+    from twisted.web.server import Site
+    from twisted.internet import reactor
+    log.startLogging(sys.stdout)
+
+    reactor.listenTCP(5000, Site(webapp.resource()))
+    wampapp.run("ws://crossbar:8080/ws", "realm1")
